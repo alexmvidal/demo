347c6003d453320c31d7b746c376f5e906324efb
alexmvidal <145687226+alexmvidal@users.noreply.github.com>
2024-09-26 12:43:06 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
-  int main()
-  {
-      char fixed_buf[10];
-      sprintf(fixed_buf,"Very long format string\n"); // Line 4. ABV.GENERAL
-      return 0;
-  }
+int main()
+{
+	char fixed_buf[10];
+	sprintf(fixed_buf,"Very long format string\n"); // Line 4. ABV.GENERAL
+	return 0;
+}
